334834be80978d6f26cf9a58b6654a29c6c70a4c
kkuez <doerping@gmx.de>
2018-09-13 10:47:05 +0200

    Erster Eintrag, einrichten

---
+++ alias/src/main/java/mdoe/example/app/App.java
@@ -0,0 +1,13 @@
+package mdoe.example.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
